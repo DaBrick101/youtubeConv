10c2c4d6c2a21f68db470dd3a9577ff56c6d8ca3
lbhbur1 <ricardo.buchebner@liebherr.com>
2023-06-06 08:33:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from pytube import YouTube
 import os
   
 yt = YouTube(
-    str(input("Enter the URL of the video you want to download: \n>> ")))
+    str(input("Link to Youtube video here man: \n>> ")))
   
 video = yt.streams.filter(only_audio=True).first()
   
-print("Enter the destination (leave blank for current directory)")
+print("Enter the destination Folder here man (leave blank for current directory):")
 destination = str(input(">> ")) or '.'
   
 out_file = video.download(output_path=destination)
@@ -15,5 +15,6 @@ base, ext = os.path.splitext(out_file)
 new_file = base + '.mp3'
 os.rename(out_file, new_file)
   
-print(yt.title + " has been successfully downloaded.")
+print(yt.title + " has been successfully downloaded bruder.")
 #Brick was here
+#
